adf47464196fab4df7e77c9bcde1fe8fb1c6244e
MiKane200 <zongwei.li@hand-china.com>
2018-07-21 12:45:58 +0800

    init

--- Exam3/src/main/java/com/hand/App.java
+++ Exam3/src/main/java/com/hand/App.java
@@ -6,7 +6,7 @@ public class App
     {
         GetInfo getInfo = GetInfo.getInstance();
         //默认是601006，这里可以加code来转换编码
-        getInfo.setCode("601006");
+        getInfo.setCode("sz300170");
         for (String arg:args) {
             System.out.println("输入的参数:"+arg);
             getInfo.setCode(arg);
